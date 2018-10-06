093cbf0ccf680283e54f70993d72b7f19433de60
David G Reidie <Daver2212@gmail.com>
2018-10-06 13:00:48 +0100

    intergration methods implemented
    need to repair euler intergration, adds energy for some reason.
    
    Task 1 and 2 complete

--- 3. Timing/main.cpp
+++ 3. Timing/main.cpp
@@ -34,7 +34,7 @@ GLfloat lastFrame = 0.0f;
 int selection;
 float mass;
 //velocity, acc, force, gravity(-9.8 on y-axis)
-glm::vec3 v, a, f, g = glm::vec3(0.0f, -9.8f, 0.0f);
+glm::vec3 f, g = glm::vec3(0.0f, -9.8f, 0.0f);
 glm::vec3 boundScale = glm::vec3(5.0f);
 
 
@@ -47,10 +47,36 @@ float randf(float lo, float hi)
 	return lo + r;
 }
 
+//Semi-implicit
+void integrate(Particle &p, glm::vec3 force, float dt)
+{
+	p.setAcc(g + force / p.getMass());
+	p.setVel(p.getVel() + p.getAcc() * dt);
+	if (p.getVel().length() < 0.05f) 
+		p.setVel(glm::vec3(0.0f, 0.0f, 0.0f));
+	p.translate(p.getVel() * dt);
+}
+
+//Euler integration
+void integrateForward(Particle &p, glm::vec3 force, float dt)
+{
+	glm::vec3 vel;
+	vel = p.getVel() + p.getAcc() * dt;
+	p.setAcc(force / p.getMass());
+	if (vel.length() < 0.05f)
+		vel = glm::vec3(0.0f, 0.0f, 0.0f);
+	p.setPos(p.getPos() + p.getVel() * dt);
+	p.setVel(vel);
+}
 
 // main function
 int main()
 {
+	std::cout << "Select 1 for Semi-implicit intergration" << std::endl;
+	std::cout << "Select 2 for Euler intergration" << std::endl;
+	std::cout << " " << std::endl;
+	std::cin >> selection;
+	
 	//Create application
 	Application app = Application::Application();
 	app.initRender();
@@ -59,7 +85,8 @@ int main()
 	//Shaders
 	Shader lambert = Shader("resources/shaders/physics.vert", "resources/shaders/physics.frag");
 	Shader transparent= Shader("resources/shaders/physics.vert", "resources/shaders/solid_transparent.frag");
-	
+	Shader particleShader = Shader("resources/shaders/solid.vert", "resources/shaders/solid_red.frag");
+
 	//Create ground plane mesh
 	Mesh plane = Mesh::Mesh(Mesh::QUAD);
 	// scale it up x5
@@ -67,13 +94,19 @@ int main()
 	//Apply Shader
 	plane.setShader(lambert);
 	
-	// create particle
-	Mesh particle1 = Mesh::Mesh("resources/models/sphere.obj");//Mesh::QUAD
-	//scale it down (x.1), translate it up by 2.5 and rotate it by 90 degrees around the x axis
-	particle1.translate(glm::vec3(0.0f, 2.5f, 0.0f));
-	particle1.scale(glm::vec3(.1f, .1f, .1f));
-	particle1.rotate((GLfloat)M_PI_2, glm::vec3(1.0f, 0.0f, 0.0f));
-	particle1.setShader(Shader("resources/shaders/solid.vert", "resources/shaders/solid_red.frag"));
+	//Create the particle
+	Particle p1 = Particle::Particle();
+	//Translate and rotate
+	p1.translate(glm::vec3(0.0f, 2.5f, 0.0f));
+	p1.rotate((GLfloat)M_PI_2, glm::vec3(1.0f, 0.0f, 0.0f));
+	//apply shader to the mesh
+	p1.getMesh().setShader(particleShader);
+
+	//Positions
+	p1.setVel(glm::vec3(randf(2.5f, 8.0f))); // (u)
+	
+	//Give particle1 some mass
+	p1.setMass(2.0f);
 
 	//Create a box for collision
 	Mesh bounds = Mesh::Mesh("resources/models/cube.obj");
@@ -88,18 +121,12 @@ int main()
 	double currentTime = (double)glfwGetTime();
 	double timeAccumulator = 0.0;
 
-	//Positions
-	v = glm::vec3(randf(2.5f, 8.0f)); // (u)
-	
 	//vars for areodrag 
 	float energyLoss = 0.70f; //Energy lost on each bounce
 	float airDens = 1.225f; //Density of the air
 	float crossSectional = 125.0f; //CrossSectional of Cube area
 	float dragCoefficient = 0.47f;//1.05-1.15 for quad shaped particle, 0.47 for sphere
 	
-	//Give particle1 some mass
-	mass = 2.0f;
-
 	// Game loop
 	while (!glfwWindowShouldClose(app.getWindow()))
 	{
@@ -119,8 +146,8 @@ int main()
 			//Press E to start again
 			if (app.keys[GLFW_KEY_E])
 			{
-				particle1.setPos(glm::vec3(randf(-2.49f, 2.49f), randf(2.5f, 4.99f), randf(-2.49f, 2.49f)));
-				v = glm::vec3(glm::vec3(randf(2.5f, 8.0f), randf(2.5f, 8.0f), randf(2.5f, 8.0f)));
+				p1.setPos(glm::vec3(randf(-2.49f, 2.49f), randf(2.5f, 4.99f), randf(-2.49f, 2.49f)));
+				p1.setVel(glm::vec3(glm::vec3(randf(2.5f, 8.0f), randf(2.5f, 8.0f), randf(2.5f, 8.0f))));
 			}
 
 			/*
@@ -129,12 +156,23 @@ int main()
 
 			//Compute Forces
 			//Areodrag
-			float fDrag = airDens * 0.5f*a.x*a.x*dragCoefficient*crossSectional;
+			float fDrag = airDens * 0.5f* p1.getAcc().x * p1.getAcc().x *dragCoefficient*crossSectional;
 			f = g + fDrag;
+
 			//Compute Acceleration
-			a = f / mass;
-			//Compute Velocity
-			v += dt * a;
+			p1.setAcc(f / p1.getMass());
+			
+			switch (selection)
+			{
+			case 1:
+				//intergrate
+				//p1.setVel(p1.getVel() + dt * p1.getAcc());
+				break;
+				integrate(p1, f, dt);
+			case 2:
+				integrateForward(p1, f, dt);
+				break;
+			}
 
 			//Get the corners of the bounds cube in a vec
 			glm::vec3 corner;
@@ -148,37 +186,41 @@ int main()
 			for (int i = 0; i < 3; i++)
 			{
 				//if particles x,y,z is further than bounds
-				if (particle1.getPos()[i] >= corner[i])
+				if (p1.getPos()[i] >= corner[i])
 				{
 					//Get the current position
-					glm::vec3 p1Pos = glm::vec3(particle1.getPos().x, particle1.getPos().y, particle1.getPos().z);
+					glm::vec3 p1Pos = p1.getPos();
 					//Set axis in contact to corners position
 					p1Pos[i] = corner[i];
+					//get velocity
+					glm::vec3 vel = p1.getVel();
 					//reverse velocity of axis
-					v[i] = -v[i];
-					//apply loss of energy
-					v *= energyLoss;
+					vel[i] = -p1.getVel()[i];
+					vel *= energyLoss;
 					//apply the change to position vector
-					particle1.setPos(p1Pos);
+					p1.setPos(p1Pos);
+					p1.setVel(vel);
 				}
-				//if particles x,y,z is less than bounds
-				else if (particle1.getPos()[i] <= (corner[i] - boundScale[i]))
+				//if particles x,y,z is less than bounds - boundScale[i];
+				else if (p1.getPos()[i] <= (corner[i] - boundScale[i]))
 				{
 					//Get the current position
-					glm::vec3 p1Pos = glm::vec3(particle1.getPos().x, particle1.getPos().y, particle1.getPos().z);
+					glm::vec3 p1Pos = p1.getPos();
 					//Set axis in contact to corners position
 					p1Pos[i] = corner[i] - boundScale[i];
+					//get velocity
+					glm::vec3 vel = p1.getVel();
 					//reverse velocity of axis
-					v[i] = -v[i];
-					//apply loss of energy
-					v *= energyLoss;
+					vel[i] = -p1.getVel()[i]; 
+					vel *= energyLoss;
 					//apply the change to position vector
-					particle1.setPos(p1Pos);
+					p1.setPos(p1Pos);
+					p1.setVel(vel);
 				}
 			}
 			
 			//Apply the translation
-			particle1.translate(dt*v);
+			p1.translate(dt * p1.getVel());
 
 			timeAccumulator -= dt;
 			time += dt;
@@ -197,7 +239,7 @@ int main()
 		// app.draw(plane);
 
 		// draw particles
-		app.draw(particle1);
+		app.draw(p1.getMesh());
 
 		//draw the bounds
 		app.draw(bounds);
@@ -207,8 +249,6 @@ int main()
 		
 		//Debug - Console information
 		std::cout << 1.0f / frameTime << " fps" << std::endl;
-		std::cout << "Acc " << a.x << std::endl;
-		std::cout << v.x << " " << v.y << " " << v.z << " " << std::endl;
 	}
 
 	app.terminate();
